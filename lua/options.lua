9f0b026d68019b1138a08aac0c368be9b16b5b8c
greed-d <deekshit.bti@gmail.com>
2025-03-22 18:24:49 +0545

    `chore` : add autocd option

--- lua/options.lua
+++ lua/options.lua
@@ -80,4 +80,38 @@ o.scrolloff = 21
 o.confirm = true
 
 o.whichwrap:append '<>[]hl'
+
+-- NOTE: This is in options because it's optional, change directory to root of current working buffer
+-- Array of file names indicating root directory. Modify to your liking.
+local root_names = { '.git', 'Makefile', 'PiPFile', 'manage.py' }
+
+-- Cache to use for speed up (at cost of possibly outdated results)
+local root_cache = {}
+
+local set_root = function()
+  -- Get directory path to start search from
+  local path = vim.api.nvim_buf_get_name(0)
+  if path == '' then
+    return
+  end
+  path = vim.fs.dirname(path)
+
+  -- Try cache and resort to searching upward for root directory
+  local root = root_cache[path]
+  if root == nil then
+    local root_file = vim.fs.find(root_names, { path = path, upward = true })[1]
+    if root_file == nil then
+      return
+    end
+    root = vim.fs.dirname(root_file)
+    root_cache[path] = root
+  end
+
+  -- Set current directory
+  vim.fn.chdir(root)
+end
+
+local root_augroup = vim.api.nvim_create_augroup('MyAutoRoot', {})
+vim.api.nvim_create_autocmd('BufEnter', { group = root_augroup, callback = set_root })
+
 -- vim: ts=2 sts=2 sw=2 et
