5b2d463f62f3cd2cf2fc5cd5b09289ca8cd14f48
greed-d <deekshit.bti@gmail.com>
2025-03-24 00:22:50 +0545

    `chore` : enable relative line number

--- lua/options.lua
+++ lua/options.lua
@@ -10,10 +10,9 @@ vim.opt_local.formatoptions:remove { 'r', 'o', 'c' } -- Stop neovim from adding 
 
 -- Make line numbers default
 o.number = true
-o.rnu = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- o.relativenumber = true
+o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 o.mouse = 'a'
@@ -83,35 +82,5 @@ o.whichwrap:append '<>[]hl'
 
 -- NOTE: This is in options because it's optional, change directory to root of current working buffer
 -- Array of file names indicating root directory. Modify to your liking.
-local root_names = { '.git', 'Makefile', 'PiPFile', 'manage.py' }
-
--- Cache to use for speed up (at cost of possibly outdated results)
-local root_cache = {}
-
-local set_root = function()
-  -- Get directory path to start search from
-  local path = vim.api.nvim_buf_get_name(0)
-  if path == '' then
-    return
-  end
-  path = vim.fs.dirname(path)
-
-  -- Try cache and resort to searching upward for root directory
-  local root = root_cache[path]
-  if root == nil then
-    local root_file = vim.fs.find(root_names, { path = path, upward = true })[1]
-    if root_file == nil then
-      return
-    end
-    root = vim.fs.dirname(root_file)
-    root_cache[path] = root
-  end
-
-  -- Set current directory
-  vim.fn.chdir(root)
-end
-
-local root_augroup = vim.api.nvim_create_augroup('MyAutoRoot', {})
-vim.api.nvim_create_autocmd('BufEnter', { group = root_augroup, callback = set_root })
 
 -- vim: ts=2 sts=2 sw=2 et
