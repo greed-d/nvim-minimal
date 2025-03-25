bb75b458b79cb023cd38a6dc7f13ecd159490b59
greed-d <deekshit.bti@gmail.com>
2025-03-26 00:26:32 +0545

    `chore` : python and typescript linter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,6 +33,8 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        typescript = { 'prettierd' },
+        python = { 'ruff' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
